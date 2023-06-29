cfe94609507006da346be23cef612d18cf844e80
getulio.souza <getulio.souza@evolutix.com.br>
2023-06-29 15:58:50 -0300

    feature: first-configurations

---
+++ app/layout.jsx
@@ -0,0 +1,19 @@
+import '../styles/globals.css'
+
+export const metadata = {
+  title: 'Menos conversinha. Mais conversão', 
+  description:'Conheça as estratégias que mudaram o jogo e como aplicá-las no seu negócio'
+}
+
+const Layout = () => {
+  return (
+    <html lang='pt-br'>
+      <body>
+        <section className='full-background'>
+        </section>
+      </body>
+    </html>
+  )
+}
+
+export default Layout;
