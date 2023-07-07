b62033115555905ca101fec200680ceab7b1ca44
getulio.souza <getulio.souza@evolutix.com.br>
2023-07-06 23:02:01 -0300

    bugfix: metadata-changed

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,9 +10,7 @@ import Footer from '@/components/6-footer/footer'
 import "../styles/globals.css";
 
 export const metadata = {
-  title: "Menos conversinha. Mais conversão",
-  description:
-    "Conheça as estratégias que mudaram o jogo e como aplicá-las no seu negócio",
+  title: "Leadster - LandingPage",
 };
 
 const Layout = () => {
